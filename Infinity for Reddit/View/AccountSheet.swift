ed10aec71ebaa5e4c2b24dd523e5322164bb5392
Docile-Alligator <25734209+Docile-Alligator@users.noreply.github.com>
2025-05-28 11:19:22 -0400

    Show profile menu and karma in AccountSheet.

--- Infinity for Reddit/View/AccountSheet.swift
+++ Infinity for Reddit/View/AccountSheet.swift
@@ -27,65 +27,88 @@ struct AccountSheet: View {
     var body: some View {
         ScrollView {
             VStack(spacing: 20) {
-                Spacer()
-                    .frame(height: 20)
-                
-                CustomWebImage(
-                    accountViewModel.account.profileImageUrl,
-                    width: 96,
-                    height: 96,
-                    circleClipped: true,
-                    fallbackView: {
-                        SwiftUI.Image(systemName: "person.crop.circle")
-                            .resizable()
-                            .frame(width: 96, height: 96)
-                    }
-                )
-                
-                Text(accountViewModel.account.isAnonymous() == true ? "Anonymous" : accountViewModel.account.username)
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                
-                ForEach(accountListingViewModel.otherAccounts, id: \.username) { account in
-                    IconTextButton(iconUrl: account.profileImageUrl ?? "", isWebImage: true, text: account.username) {
-                        do {
-                            AccountViewModel.shared.switchAccount(newAccount: account)
-                            try AccountViewModel.shared.updateTokens(accessToken: account.accessToken ?? "", refreshToken: account.refreshToken ?? "")
+                ZStack {
+                    CustomWebImage(
+                        accountViewModel.account.bannerImageUrl,
+                        height: 150,
+                        handleImageTapGesture: false
+                    )
+                    
+                    CustomWebImage(
+                        accountViewModel.account.profileImageUrl,
+                        width: 96,
+                        height: 96,
+                        circleClipped: true,
+                        handleImageTapGesture: false,
+                        fallbackView: {
+                            SwiftUI.Image(systemName: "person.crop.circle")
+                                .resizable()
+                                .frame(width: 96, height: 96)
                         }
-                        catch{
-                            print("Error: switching account failed")
-                        }
+                    )
+                }
+                
+                VStack(spacing: 20) {
+                    VStack {
+                        Text(accountViewModel.account.isAnonymous() == true ? "Anonymous" : accountViewModel.account.username)
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
                         
-                        dismiss()
-                    }
-                }
-                
-                IconTextButton(iconUrl: "person.crop.circle.badge.plus", text: "Add account") {
-                    dismiss()
-                    navigationManager.path.append(AppNavigation.login)
-                }
-                
-                if accountViewModel.account.isAnonymous() == false {
-                    IconTextButton(iconUrl: "person.fill.questionmark", text: "Anonymous") {
-                        do {
-                            try accountViewModel.switchToAnonymous()
-                        } catch {
-                            print("Failed to log out: \(error)")
+                        if accountViewModel.account.isAnonymous() != true {
+                            Text("Karma: \(accountViewModel.account.karma)")
+                                .primaryText()
                         }
-                        dismiss()
                     }
                     
-                    IconTextButton(iconUrl: "rectangle.portrait.and.arrow.right", text: "Log out") {
-                        do {
-                            try accountViewModel.logout()
-                        } catch {
-                            print("Failed to log out: \(error)")
+                    if accountViewModel.account.isAnonymous() != true {
+                        IconTextButton(iconUrl: "person.crop.circle", text: "Profile") {
+                            dismiss()
+                            navigationManager.path.append(AppNavigation.userDetails(username: accountViewModel.account.username))
                         }
+                    }
+                    
+                    ForEach(accountListingViewModel.otherAccounts, id: \.username) { account in
+                        IconTextButton(iconUrl: account.profileImageUrl ?? "", isWebImage: true, text: account.username) {
+                            do {
+                                AccountViewModel.shared.switchAccount(newAccount: account)
+                                try AccountViewModel.shared.updateTokens(accessToken: account.accessToken ?? "", refreshToken: account.refreshToken ?? "")
+                            }
+                            catch{
+                                print("Error: switching account failed")
+                            }
+                            
+                            dismiss()
+                        }
+                    }
+                    
+                    IconTextButton(iconUrl: "person.crop.circle.badge.plus", text: "Add account") {
                         dismiss()
+                        navigationManager.path.append(AppNavigation.login)
+                    }
+                    
+                    if accountViewModel.account.isAnonymous() == false {
+                        IconTextButton(iconUrl: "person.fill.questionmark", text: "Anonymous") {
+                            do {
+                                try accountViewModel.switchToAnonymous()
+                            } catch {
+                                print("Failed to log out: \(error)")
+                            }
+                            dismiss()
+                        }
+                        
+                        IconTextButton(iconUrl: "rectangle.portrait.and.arrow.right", text: "Log out") {
+                            do {
+                                try accountViewModel.logout()
+                            } catch {
+                                print("Failed to log out: \(error)")
+                            }
+                            dismiss()
+                        }
                     }
                 }
+                .padding(.horizontal, 24)
             }
         }
-        .padding(.horizontal, 24)
+        //.padding(.horizontal, 24)
     }
 }
