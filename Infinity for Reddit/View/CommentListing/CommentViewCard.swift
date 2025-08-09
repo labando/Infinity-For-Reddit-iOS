5fa7d6532b68974ddf97758cb1533e2f57505360
Docile-Alligator <25734209+Docile-Alligator@users.noreply.github.com>
2025-08-08 23:18:25 -0400

    Bare bone fully collapsing comment. Settings is not available.

--- Infinity for Reddit/View/CommentListing/CommentViewCard.swift
+++ Infinity for Reddit/View/CommentListing/CommentViewCard.swift
@@ -10,11 +10,14 @@ import SDWebImageSwiftUI
 import MarkdownUI
 
 struct CommentViewCard: View {
+    @EnvironmentObject var customThemeViewModel: CustomThemeViewModel
     @EnvironmentObject var navigationManager: NavigationManager
     
     @StateObject var commentViewModel: CommentViewModel
     @State private var voteTask: Task<Void, Never>? = nil
     @State private var saveTask: Task<Void, Never>? = nil
+    
+    @State private var fullyCollapseComment: Bool = true
     
     let formatter = DateFormatter()
     private let isInPostDetails: Bool
@@ -79,101 +82,104 @@ struct CommentViewCard: View {
                 .padding(.top, 8)
                 .padding(.bottom, 8)
                 
-                Group {
-                    if commentViewModel.comment.bodyProcessedMarkdown != nil {
-                        Markdown(commentViewModel.comment.bodyProcessedMarkdown!)
-                            .markdownImageProvider(WebImageProvider(mediaMetadata: commentViewModel.comment.mediaMetadata))
-                            .font(.system(size: 24))
-                            .padding(.horizontal, 16)
-                            .themedCommentMarkdown()
-                            .markdownLinkHandler { url in
-                                LinkHandler.shared.handle(url: url)
-                            }
-                            .id(commentViewModel.comment.id)
-                    } else {
-                        Markdown(commentViewModel.comment.body)
-                            .markdownImageProvider(WebImageProvider(mediaMetadata: commentViewModel.comment.mediaMetadata))
-                            .font(.system(size: 24))
-                            .padding(.horizontal, 16)
-                            .themedCommentMarkdown()
-                            .markdownLinkHandler { url in
-                                LinkHandler.shared.handle(url: url)
-                            }
-                            .id(commentViewModel.comment.id)
-                    }
-                }
-                
-                HStack(alignment: .center) {
-                    Button(action: {
-                        voteTask?.cancel()
-                        voteTask = Task {
-                            await commentViewModel.voteComment(vote: 1)
+                if !(commentViewModel.comment.isCollasped && fullyCollapseComment) {
+                    Group {
+                        if commentViewModel.comment.bodyProcessedMarkdown != nil {
+                            Markdown(commentViewModel.comment.bodyProcessedMarkdown!)
+                                .markdownImageProvider(WebImageProvider(mediaMetadata: commentViewModel.comment.mediaMetadata))
+                                .font(.system(size: 24))
+                                .padding(.horizontal, 16)
+                                .themedCommentMarkdown()
+                                .markdownLinkHandler { url in
+                                    LinkHandler.shared.handle(url: url)
+                                }
+                                .id(commentViewModel.comment.id)
+                        } else {
+                            Markdown(commentViewModel.comment.body)
+                                .markdownImageProvider(WebImageProvider(mediaMetadata: commentViewModel.comment.mediaMetadata))
+                                .font(.system(size: 24))
+                                .padding(.horizontal, 16)
+                                .themedCommentMarkdown()
+                                .markdownLinkHandler { url in
+                                    LinkHandler.shared.handle(url: url)
+                                }
+                                .id(commentViewModel.comment.id)
                         }
-                    }) {
-                        SwiftUI.Image(systemName: commentViewModel.comment.likes == 1 ? "arrowshape.up.fill" : "arrowshape.up")
-                            .commentIconTemplateRendering()
-                            .commentUpvoteIcon(isUpvoted: commentViewModel.comment.likes == 1)
                     }
-                    .buttonStyle(.borderless)
                     
-                    Text(String(commentViewModel.comment.score + commentViewModel.comment.likes))
-                        .frame(width: 50, alignment: .center)
-                        .commentInfo()
-                    
-                    Button(action: {
-                        voteTask?.cancel()
-                        voteTask = Task {
-                            await commentViewModel.voteComment(vote: -1)
-                        }
-                    }) {
-                        SwiftUI.Image(systemName: commentViewModel.comment.likes == -1 ? "arrowshape.down.fill" : "arrowshape.down")
-                            .commentIconTemplateRendering()
-                            .commentDownvoteIcon(isDownvoted: commentViewModel.comment.likes == -1)
-                    }
-                    .padding(.trailing, 16)
-                    .buttonStyle(.borderless)
-                    
-                    Spacer()
-                    
-                    if let onToggleExpand, commentViewModel.comment.replies?.comments?.count ?? -1 > 0 {
+                    HStack(alignment: .center) {
                         Button(action: {
-                            onToggleExpand()
+                            voteTask?.cancel()
+                            voteTask = Task {
+                                await commentViewModel.voteComment(vote: 1)
+                            }
                         }) {
-                            SwiftUI.Image(systemName: "chevron.up")
+                            SwiftUI.Image(systemName: commentViewModel.comment.likes == 1 ? "arrowshape.up.fill" : "arrowshape.up")
                                 .commentIconTemplateRendering()
-                                .commentIcon()
-                                .rotationEffect(.degrees(commentViewModel.comment.isCollasped ? 180 : 0))
-                                .animation(.easeInOut(duration: 0.25), value: commentViewModel.comment.isCollasped)
+                                .commentUpvoteIcon(isUpvoted: commentViewModel.comment.likes == 1)
+                        }
+                        .buttonStyle(.borderless)
+                        
+                        Text(String(commentViewModel.comment.score + commentViewModel.comment.likes))
+                            .frame(width: 50, alignment: .center)
+                            .commentInfo()
+                        
+                        Button(action: {
+                            voteTask?.cancel()
+                            voteTask = Task {
+                                await commentViewModel.voteComment(vote: -1)
+                            }
+                        }) {
+                            SwiftUI.Image(systemName: commentViewModel.comment.likes == -1 ? "arrowshape.down.fill" : "arrowshape.down")
+                                .commentIconTemplateRendering()
+                                .commentDownvoteIcon(isDownvoted: commentViewModel.comment.likes == -1)
                         }
                         .padding(.trailing, 16)
                         .buttonStyle(.borderless)
-                    }
-                    
-                    Button(action: {
-                        saveTask?.cancel()
-                        saveTask = Task {
-                            await commentViewModel.saveComment(save: !commentViewModel.comment.saved)
+                        
+                        Spacer()
+                        
+                        if let onToggleExpand, commentViewModel.comment.replies?.comments?.count ?? -1 > 0 {
+                            Button(action: {
+                                onToggleExpand()
+                            }) {
+                                SwiftUI.Image(systemName: "chevron.up")
+                                    .commentIconTemplateRendering()
+                                    .commentIcon()
+                                    .rotationEffect(.degrees(commentViewModel.comment.isCollasped ? 180 : 0))
+                                    .animation(.easeInOut(duration: 0.25), value: commentViewModel.comment.isCollasped)
+                            }
+                            .padding(.trailing, 16)
+                            .buttonStyle(.borderless)
                         }
-                    }) {
-                        SwiftUI.Image(systemName: commentViewModel.comment.saved ? "bookmark.fill" : "bookmark")
-                            .commentIconTemplateRendering()
-                            .commentIcon()
+                        
+                        Button(action: {
+                            saveTask?.cancel()
+                            saveTask = Task {
+                                await commentViewModel.saveComment(save: !commentViewModel.comment.saved)
+                            }
+                        }) {
+                            SwiftUI.Image(systemName: commentViewModel.comment.saved ? "bookmark.fill" : "bookmark")
+                                .commentIconTemplateRendering()
+                                .commentIcon()
+                        }
+                        .padding(.trailing, 16)
+                        .buttonStyle(.borderless)
+                        
+                        ShareLink(item: "https://reddit.com" + commentViewModel.comment.permalink) {
+                            SwiftUI.Image(systemName: "square.and.arrow.up")
+                                .commentIconTemplateRendering()
+                                .commentIcon()
+                        }
+                        .buttonStyle(.borderless)
                     }
-                    .padding(.trailing, 16)
-                    .buttonStyle(.borderless)
-                    
-                    ShareLink(item: "https://reddit.com" + commentViewModel.comment.permalink) {
-                        SwiftUI.Image(systemName: "square.and.arrow.up")
-                            .commentIconTemplateRendering()
-                            .commentIcon()
-                    }
-                    .buttonStyle(.borderless)
+                    .padding(.horizontal, 16)
+                    .padding(.top, 8)
+                    .padding(.bottom, 8)
                 }
-                .padding(.horizontal, 16)
-                .padding(.top, 8)
-                .padding(.bottom, 8)
             }
         }
         .contentShape(Rectangle())
+        .background(commentViewModel.comment.isCollasped && fullyCollapseComment ? Color(hex: customThemeViewModel.currentCustomTheme.fullyCollapsedCommentBackgroundColor) : Color.clear)
     }
 }
