7c0119a845d1f422654dd29228ad11cc99993f0c
Docile-Alligator <25734209+Docile-Alligator@users.noreply.github.com>
2025-08-27 20:42:16 -0400

    Continue implementing snackbar.

--- Infinity for Reddit/View/CustomView/Snackbar.swift
+++ Infinity for Reddit/View/CustomView/Snackbar.swift
@@ -10,6 +10,11 @@ import SwiftUI
 struct Snackbar: View {
     @EnvironmentObject private var snackbarManager: SnackbarManager
     
+    @State private var dragOffset: CGSize = .zero
+    @State private var isVisible = true
+    
+    private let dismissThreadshold: CGFloat = 80
+    
     var body: some View {
         if snackbarManager.showSnackbar {
             VStack {
@@ -35,6 +40,32 @@ struct Snackbar: View {
                         .fill(Color(hex: "#353E41"))
                 )
                 .padding(16)
+                .offset(x: dragOffset.width)
+                // To make the animation smoother
+                .opacity(isVisible ? 1 : 0)
+                .animation(.easeInOut(duration: 0.3), value: isVisible)
+                .gesture(
+                    DragGesture()
+                        .onChanged { value in
+                            dragOffset = value.translation
+                        }
+                        .onEnded { value in
+                            if abs(value.translation.width) > dismissThreadshold {
+                                withAnimation(.snappy(duration: 0.3)) {
+                                    dragOffset = CGSize(width: value.translation.width * 3, height: 0)
+                                    isVisible = false
+                                } completion: {
+                                    snackbarManager.dismiss()
+                                    dragOffset = .zero
+                                    isVisible = true
+                                }
+                            } else {
+                                withAnimation(.spring()) {
+                                    dragOffset = .zero
+                                }
+                            }
+                        }
+                )
             }
         }
     }
